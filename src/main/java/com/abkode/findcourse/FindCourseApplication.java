3fa7168f31d686d43a94fafdc77286ddbd5b7777
Ali Büyükkaytan <a.buyukkaytan@gmail.com>
2022-04-18 15:45:31 +0300

    first commit

---
+++ src/main/java/com/abkode/findcourse/FindCourseApplication.java
@@ -0,0 +1,13 @@
+package com.abkode.findcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abkode/findcourse/FindCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abkode.findcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
