1433d1ec1a0b0d476ddc9d62bf8ed3d51b739c0d
Ali Büyükkaytan <a.buyukkaytan@gmail.com>
2022-04-20 02:18:56 +0300

    ModelMapper bean created to use to map objects in service steps

---
+++ src/main/java/com/abkode/findcourse/bean/ModelMapperBean.java
@@ -0,0 +1,11 @@
+package com.abkode.findcourse.bean;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperBean {
+    @Bean
+    public ModelMapper modelMapper(){return new ModelMapper();}
+}
