235f64edac734a50f108dd82e3ac83fdb5143212
Ali Büyükkaytan <a.buyukkaytan@gmail.com>
2022-04-22 14:24:08 +0300

    after changes codes became more clean

--- src/main/java/com/abkode/findcourse/ui/rest/CourseController.java
+++ src/main/java/com/abkode/findcourse/ui/rest/CourseController.java
@@ -1,16 +1,9 @@
 package com.abkode.findcourse.ui.rest;
-
 import com.abkode.findcourse.business.dto.CourseDto;
-import com.abkode.findcourse.business.services.CourseService;
 import com.abkode.findcourse.business.services.impl.CourseServiceImpl;
-import com.abkode.findcourse.data.entity.CourseEntity;
-import com.abkode.findcourse.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,53 +11,42 @@ import java.util.Map;
 @RequestMapping("/api/v1")
 public class CourseController {
 
+
     @Autowired
-    private CourseService courseService;
+    private CourseServiceImpl courseServiceImpl;
 
     //LIST
     //http://localhost:8080/api/v1/courses
     @GetMapping("/courses")
     public List<CourseDto> getAllCourse() {
-        List<CourseDto> courseDtoList = courseService.getAllCourse();
-        return courseDtoList;
+        return courseServiceImpl.getAllCourse();
     }
 
     //SAVE
     //http://localhost:8080/api/v1/courses
     @PostMapping("/courses")
     public CourseDto createCourse(@RequestBody CourseDto courseDto) {
-        courseService.createCourse(courseDto);
-        return courseDto;
+        return courseServiceImpl.createCourse(courseDto);
     }
-
 
     //FIND
     //http://localhost:8080/api/v1/courses/1
     @GetMapping("/courses/{id}")
     public ResponseEntity<CourseDto> getCourseById(@PathVariable Long id) {
-        ResponseEntity<CourseDto> courseDto = courseService.getCourseById(id);
-        return courseDto;
+        return courseServiceImpl.getCourseById(id);
     }
-
 
     //UPDATE
     //http://localhost:8080/api/v1/courses/1
     @PutMapping("/courses/{id}")
     public ResponseEntity<CourseDto> updateCourse(@PathVariable Long id,@RequestBody  CourseDto courseDto) {
-        return courseService.updateCourse(id, courseDto);
+        return courseServiceImpl.updateCourse(id, courseDto);
     }
-
 
     //DELETE
     //http://localhost:8080/api/v1/courses/1
     @DeleteMapping("/courses/{id}")
     public ResponseEntity<Map<String, Boolean>> deleteCourseById(@PathVariable Long id) {
-
-        return courseService.deleteCourseById(id);
-
-        /*Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
-        return ResponseEntity.ok(response);*/
-
+        return courseServiceImpl.deleteCourseById(id);
     }
 }
--- src/main/java/com/abkode/findcourse/ui/rest/StudentController.java
+++ src/main/java/com/abkode/findcourse/ui/rest/StudentController.java
@@ -1,13 +1,11 @@
 package com.abkode.findcourse.ui.rest;
 
-
 import com.abkode.findcourse.business.dto.StudentDto;
-import com.abkode.findcourse.business.services.StudentService;
+import com.abkode.findcourse.business.services.impl.StudentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,13 +14,12 @@ import java.util.Map;
 public class StudentController {
 
     @Autowired
-    private StudentService studentService;
-
+    private StudentServiceImpl studentServiceImpl;
 
     //ROOT
     //http://localhost:8080/api/v1/index
-    @GetMapping({"/index","/"}  )
-    public String getRoot(){
+    @GetMapping({"/index", "/"})
+    public String getRoot() {
         return "index";
     }
 
@@ -30,49 +27,36 @@ public class StudentController {
     //http://localhos:8080/api/v1/students
     @GetMapping("/students")
     public List<StudentDto> getAllStudents() {
-        List<StudentDto> studentListDto = studentService.getAllStudents();
-        return studentListDto;
+        return studentServiceImpl.getAllStudents();
     }
-
 
     //SAVE
     //http://localhost:8080/api/v1/students
     @PostMapping("/students")
     public StudentDto createStudent(@RequestBody StudentDto studentDto) {
-        studentService.createStudent(studentDto);
-        return studentDto;
+        return studentServiceImpl.createStudent(studentDto);
     }
-
 
     //FIND
     //http://localhost:8080/api/v1/students/1
     @GetMapping("/students/{id}")
     public ResponseEntity<StudentDto> getStudentById(@PathVariable Long id) {
-        ResponseEntity<StudentDto> studentDto = studentService.getStudentById(id);
-        return studentDto;
-    }
+        return studentServiceImpl.getStudentById(id);
 
+    }
 
     //UPDATE
     //http://localhost:8080/api/v1/students/1
     @PutMapping("/students/{id}")
-    public ResponseEntity<StudentDto> updateStudent(@PathVariable Long id,@RequestBody StudentDto studentDto) {
-        studentService.updateStudent(id, studentDto);
-        return ResponseEntity.ok(studentDto);
+    public ResponseEntity<StudentDto> updateStudent(@PathVariable Long id, @RequestBody StudentDto studentDto) {
+        return studentServiceImpl.updateStudent(id, studentDto);
     }
-
 
     //DELETE
     //http://localhost:8080/api/v1/students/1
     @DeleteMapping("/students/{id}")
     public ResponseEntity<Map<String, Boolean>> deleteStudentById(@PathVariable Long id) {
-
-        studentService.deleteStudentById(id);
-
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
-        return ResponseEntity.ok(response);
-
+        return studentServiceImpl.deleteStudentById(id);
     }
 
 }
