e38a5712f87fa9200315aae68856215b2eca443d
Ali Büyükkaytan <a.buyukkaytan@gmail.com>
2022-06-30 23:37:00 +0300

    removed security files

--- src/main/java/com/abkode/findcourse/security/jwt/_03_UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abkode.findcourse.security.jwt;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface _03_UserRepository extends CrudRepository<_02_UserEntity,Long> {
-    _02_UserEntity findByUsername(String username);
-}
